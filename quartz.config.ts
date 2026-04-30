3e63851c3fb950212f689a2ce0216dbfe38e6a98
Sagiv <sagivlevi2002@gmail.com>
2026-04-30 15:36:24 +0300

    fix base url

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "https://github.com/VLevi2611/obsidian-quartz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
